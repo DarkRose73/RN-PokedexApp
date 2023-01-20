f1f62955323c7f150e50af453c646ca2ee7ad61b
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-20 13:32:58 -0300

    initial commit

---
+++ src/screens/PokemonScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const PokemonScreen = () => {
+  return (
+    <View>
+      <Text>PokemonScreen</Text>
+    </View>
+  );
+};
